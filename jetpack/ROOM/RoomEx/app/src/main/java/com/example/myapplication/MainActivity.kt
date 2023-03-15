2bcfa350667a8ed5a2b6d049d91c8ca035657eb4
muldaeng <alstnwkd990@naver.com>
2023-03-16 00:14:24 +0900

    [REF] RoomDB Exercise 1

--- jetpack/ROOM/RoomEx/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ jetpack/ROOM/RoomEx/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val db = TextDatabase.getDatabase(this)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            db.textDao().insert(TextEntity(0, "asdf"))
+            Log.d("MainActivity", db.textDao().getAllData().toString())
+        }
     }
 }
