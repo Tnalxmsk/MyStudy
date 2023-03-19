68e0198947e16d3e7f41c4a28563e094d449c180
muldaeng <alstnwkd990@naver.com>
2023-03-19 16:29:52 +0900

    [ADD] Work Manager Setting

--- jetpack/WorkMananger/WorkManagerEx/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ jetpack/WorkMananger/WorkManagerEx/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.myapplication
 
-import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        SimpleThread().start()
+
+        val workManagerA = OneTimeWorkRequestBuilder<WorkManagerA>().build()
+        WorkManager.getInstance(this).enqueue(workManagerA)
+    }
+}
+
+class SimpleThread : Thread() {
+    override fun run() {
+        super.run()
+
+        for (i in 1..10) {
+            Log.d("MainActivity", "$i")
+            sleep(1000)
+        }
     }
 }
