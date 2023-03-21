44b727218e72b41334d3bcc9559981d90bedfb22
muldaeng <alstnwkd990@naver.com>
2023-03-21 18:51:56 +0900

    [ADD] Work Manager Coroutine

---
+++ jetpack/WorkMananger/WorkManagerCoroutine/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val workManager = OneTimeWorkRequestBuilder<WorkManager2>().build()
+        WorkManager.getInstance(this).enqueue(workManager)
+    }
+}
